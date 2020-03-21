19bbbea308bee741b2c14c5c5be43df8b4dff314
Paul <gizettelagrenade77@gmail.com>
2020-03-21 21:19:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -20,6 +20,9 @@ bot.on('message', async (msg) => {
         args = cmdArray.slice(1)
 
         msg.channel.send(cmd)
+        for(let i = 0; i < args.length; i++){
+            msg.channel.send(args[i])
+        }
     }
 });
 
